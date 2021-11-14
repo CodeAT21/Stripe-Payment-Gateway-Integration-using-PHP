21d203832c756ebc22a410e3e0f40d29845633e0
CodeAT21 <79390020+CodeAT21@users.noreply.github.com>
2021-11-14 09:44:33 +0530

    Add files via upload

---
+++ stripe/config.php
@@ -0,0 +1,20 @@
+<?php 
+// Product Details 
+// Minimum amount is $0.50 US 
+// Test Stripe API configuration 
+
+define('STRIPE_API_KEY', 'sk_test_jXnbWYmNh6227VACFxp1gJog');  
+define('STRIPE_PUBLISHABLE_KEY', 'pk_test_d8GOWxzerUcB34ENyGG4fGi6'); 
+
+define('STRIPE_SUCCESS_URL', 'http://localhost/stripe/success.php'); 
+define('STRIPE_CANCEL_URL', 'http://localhost/stripe/cancel.php'); 
+
+// Database configuration   
+define('DB_HOST', 'localhost');  
+define('DB_USERNAME', 'root');  
+define('DB_PASSWORD', '');  
+define('DB_NAME', 'demos'); 
+?>
+
+
+
